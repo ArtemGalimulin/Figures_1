fc14c6882778936a75e9f1bcbbf5d7ff348cbaa4
katinartem <artemka6207@mail.ru>
2025-10-29 23:42:01 +0300

    Add files via upload

--- Figures_1/Polygon.cpp
+++ Figures_1/Polygon.cpp
@@ -6,7 +6,8 @@
 #include <cmath>
 
 class Polygon : public Figure {
-	//too hard
+	double virtual perimeter() const = 0;
+	double virtual area() const = 0;
 };
 
 class Triangle : public Polygon {
@@ -51,9 +52,28 @@ public:
 };
 
 class Parallelogram : public Polygon {
-
+private:
+	double a, b, h;
+public:
+	Parallelogram(double s1, double s2, double s3) : a(s1) , b(s2), h(s3){};
+	Parallelogram() = default;
+	double area() const override {
+		return a * h;;
+	}
+	double perimeter() const override {
+		return (a + b) * 2;
+	}
 };
 
 class Rhomb : public Parallelogram {
-
+private:
+	double a, d1, d2;
+public:
+	Rhomb(double s1, double s2, double s3) : a(s1), d1(s2), d2(s3) {};
+	double area() const override {
+		return 0.5 * d1 * d2;
+	}
+	double perimeter() const override {
+		return a * 4;
+	}
 };
--- Figures_1/Polygon.h
+++ Figures_1/Polygon.h
@@ -4,3 +4,5 @@ class Polygon {};
 class Triangle {};
 class Rectangle {};
 class Square {};
+class Parallelogram {};
+class Rhomb {};
